2a2c35b75db4f0794e86a482eca88b2fadda4dca
shankyknecht <roy_veijk@hotmail.com>
2024-06-06 10:21:07 +0200

    bestand tijdelijk.py gecodeerd

---
+++ tijdelijk.py
@@ -0,0 +1,14 @@
+mijn_prijzen = {"aardbei" : "3", "vanille" : "4", "chocolade" : "5"}
+aanbieding = (4*0.8)
+reclame_tekst =(f"vandaag in de aanbieding: vanille-ijs, 1 liter - slechts ${aanbieding}")
+
+reclame_tekst2 = (reclame_tekst[:37])
+
+reclame_tekst3 = (reclame_tekst2).upper()
+
+reclame_tekst4 = reclame_tekst3
+for l in reclame_tekst4:
+
+for el in range(l[0:37]).lower:
+
+if el in range is [0:5].lower
